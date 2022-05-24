cf72931a377e97a06039a6898abfb2ad9f8a0ab8
NicolaCortese <nicolacortese92@gmail.com>
2022-05-24 17:04:10 +0100

    added the chitter controller

--- backend/api/chitters.route.js
+++ backend/api/chitters.route.js
@@ -1,7 +1,8 @@
 import express from "express"
+import ChittersController from "./chitters.controller.js"
 
 const router = express.Router()
 
-router.route("/").get((req, res) => res.send("hello world"))
+router.route("/").get(ChittersController.apiGetChitters)
 
 export default router
